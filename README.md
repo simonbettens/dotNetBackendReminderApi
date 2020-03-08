8ddb095c9e2f64c0fa076e24fc9cb398ea1bd7cb
Simon Bettens <simon.bettens@student.hogent.be>
2020-03-08 12:03:59 +0100

    #1 Start project (added controller class and models)

---
+++ README.md
@@ -0,0 +1,6 @@
+# ReminderApi
+Web Api for Web IV 
+
+Angular forend en asp.NET core api backend webapplicatie
+
+Begonnen op 8/3/2020
---
+++ ReminderApi/ReminderApi/Models/Reminder.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReminderApi.Models
+{
+    public class Reminder
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime DatumReleased { get; set; }
+        public bool Watched { get; set; }
+        public string Description { get; set; }
+        public ICollection<Tag> Tags { get; private set; }
+        #endregion
+
+        #region Constructors
+        public Reminder()
+        {
+            this.Tags = new List<Tag>();
+        }
+        public Reminder(string title, DateTime date , string decr,bool watched = false)
+        {
+            this.Title = title;
+            this.DatumReleased = date;
+            this.Description = decr;
+            this.Watched = watched;
+            this.Tags = new List<Tag>();
+        }
+        #endregion
+
+        #region Methodes
+        public void AddTag(Tag tag) {
+            if (!Tags.Any(t => t.Naam.Equals(tag.Naam)))
+            {
+                this.Tags.Add(tag);
+            }
+        }
+        public void RemoveTag(Tag tag) {
+            if (Tags.Any(t => t.Naam.Equals(tag.Naam)))
+            {
+                this.Tags.Remove(tag);
+            }
+        }
+        public void HasBeenWatched() {
+            this.Watched = true;
+        }
+        #endregion
+
+
+
+    }
+}
