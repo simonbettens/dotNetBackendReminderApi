320fbe52f3b0efdabe764eb4dab1e3c6b56bedc3
Simon Bettens <simon.bettens@student.hogent.be>
2020-03-29 22:47:33 +0200

    #8 added screenshots

--- ReminderApi/ReminderApi/Controllers/TagsController.cs
+++ ReminderApi/ReminderApi/Controllers/TagsController.cs
@@ -12,6 +12,7 @@ namespace ReminderApi.Controllers
     [Route("api/[controller]")]
     [ApiController]
     [Produces("application/json")]
+    [ApiConventionType(typeof(DefaultApiConventions))]
     public class TagsController : ControllerBase
     {
 
@@ -22,11 +23,22 @@ namespace ReminderApi.Controllers
         }
 
         #region Get
+        // [Get] /api/Tags 
+        /// <summary>
+        /// returns all tags 
+        /// </summary>
+        /// <returns>list of tags</returns>
         [HttpGet]
         public IEnumerable<Tag> GetTags()
         {
             return _tagRepository.GetAll().ToList();
         }
+        // [Get] /api/Tags/{id}
+        /// <summary>
+        /// Gets a tag by it's id 
+        /// </summary>
+        /// <param name="id">the id of the tag</param>
+        /// <returns>returns a tag </returns>
         [HttpGet("{id}")]
         public ActionResult<Tag> GetTag(int id)
         {
@@ -41,6 +53,12 @@ namespace ReminderApi.Controllers
         #endregion
 
         #region Post & Put & Delete Tag
+        // [Post] /api/Tags
+        /// <summary>
+        /// adds a tag to the list
+        /// </summary>
+        /// <param name="tagDTO">the dto of tag</param>
+        /// <returns>returns a the created tag</returns>
         [HttpPost]
         public ActionResult<Tag> PostTag(TagDTO tagDTO)
         {
@@ -54,6 +72,13 @@ namespace ReminderApi.Controllers
             _tagRepository.SaveChanges();
             return CreatedAtAction(nameof(GetTag), new { id = createTag.TagId }, createTag);
         }
+        // [Put] /api/Tags/{id}
+        /// <summary>
+        /// updates an existing tag
+        /// </summary>
+        /// <param name="id">the id</param>
+        /// <param name="tag">the tag</param>
+        /// <returns>if id doesn't equal the tagid a badrequest wil be returned else a nocontent</returns>
         [HttpPut("{id}")]
         public IActionResult PutTag(int id, Tag tag)
         {
@@ -65,7 +90,12 @@ namespace ReminderApi.Controllers
             _tagRepository.SaveChanges();
             return NoContent();
         }
-        
+        // [Delete] /api/Tags/{id}
+        /// <summary>
+        /// Deletes a tag
+        /// </summary>
+        /// <param name="id">the tagid</param>
+        /// <returns>if id doesn't equal the tagid a badrequest wil be returned else a nocontent</returns>
         [HttpDelete("{id}")]
         public IActionResult DeleteTag(int id)
         {
